f8442594738246d8c406076498db493c3c43d692
Nevio Werlen <LordNevi@outlook.com>
2023-11-23 12:03:46 +0100

    Add ToDo's

--- src/js/note.js
+++ src/js/note.js
@@ -2,3 +2,8 @@ function adjustTextareaHeight(textarea) {
     textarea.style.height = 'auto';
     textarea.style.height = textarea.scrollHeight + 'px';
 }
+
+$(document).on('click', '.ToDoCheckbox', function() {
+    var childInputs = $(this).siblings('.ToDoInput');
+    childInputs.toggleClass('ToDoInputChecked');
+});
